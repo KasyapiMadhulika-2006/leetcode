4dd3e84945300631eeac4cecbf1401fd55a45557
KasyapiMadhulika-2006 <23951A6757@iare.ac.in>
2025-07-15 19:24:20 +0530

    Time: 1 ms (99.12%), Space: 42.4 MB (26.49%) - LeetHub

---
+++ 3396-valid-word/3396-valid-word.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isValid(String word) {
+        if(word.length() < 3) return false;
+        boolean hasVowel = false;
+        boolean hasConsonant = false;
+        for(int i = 0; i < word.length();i++){
+            char c = word.charAt(i);
+            if(!Character.isLetterOrDigit(c)){
+                return false;
+            }
+            if(Character.isLetter(c)){
+                char lower = Character.toLowerCase(c);
+                if("aeiou".indexOf(lower) >= 0){
+                    hasVowel = true;
+                } else {
+                    hasConsonant = true;
+                }
+            }
+        }
+        return hasVowel && hasConsonant;
+    }
+}
