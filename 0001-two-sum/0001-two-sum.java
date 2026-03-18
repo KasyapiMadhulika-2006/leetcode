152f67d61fc492267afd03b4228b89080bf04e6c
KasyapiMadhulika-2006 <23951A6757@iare.ac.in>
2026-03-19 00:03:02 +0530

    Time: 2 ms (99.22%), Space: 47.1 MB (69.24%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,13 +1,20 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int n = nums.length;
-        for(int i = 0;i <= n-1;i++){
-            for(int j = i+1;j<n;j++){
-                if(nums[i] + nums[j] == target){
-                    return new int[]{i,j};
-                }
+        // Creating an hashmap to insert new indices 
+        Map<Integer, Integer> map = new HashMap<>();
+        // Iterate through the array
+        for(int i = 0;i < nums.length;i++){
+            // caluculating the complement to see the number to be present or not
+            int complement = target - nums[i];
+            // Check if the complement is already in the map
+            if (map.containsKey(complement)){
+                // if found then return the indices of the complemtn
+                return new int[] {map.get(complement), i};
             }
+            //Otherwise add into the map
+            map.put(nums[i],i);
         }
+        //Return an empty array if no solution is found
         return new int[] {};
     }
 }
