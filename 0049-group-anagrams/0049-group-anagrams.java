6693c59cf660111278348e1b321f23d3dce91e20
KasyapiMadhulika-2006 <23951A6757@iare.ac.in>
2026-03-20 12:58:36 +0530

    Time: 21 ms (10.83%), Space: 49.9 MB (34.56%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,16 +1,27 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-     Map<String,List<String>> map = new HashMap<>();
-     for(String s:strs){
-        char[] arr = s.toCharArray();
-        Arrays.sort(arr);
-        String key = new String(arr);
-        if(!map.containsKey(key)){
-            map.put(key,new ArrayList<>());
-
+        if(strs.length== 0){
+            return new ArrayList<>();
         }
-        map.get(key).add(s);
-     }   
-     return new  ArrayList<>(map.values());
+        Map<String,List> ansMap = new HashMap<>();
+        for(String s:strs){
+             int[] count = new int[26];
+            Arrays.fill(count,0);
+            for(char c : s.toCharArray()){
+                count[c - 'a']++;
+            }
+            StringBuilder sb = new StringBuilder("");
+
+            for(int i = 0; i < 26;i++){
+                sb.append("#");
+                sb.append(count[i]);
+            }
+            String key= sb.toString();
+            if(!ansMap.containsKey(key)){
+                ansMap.put(key,new ArrayList<>());
+            }
+              ansMap.get(key).add(s);
+        }
+           return new ArrayList(ansMap.values());
     }
 }
